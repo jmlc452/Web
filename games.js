974ec57a2a7ff844b579d9f547c82000daf71cf9
jmlc452 <71135625+jmlc452@users.noreply.github.com>
2020-10-14 11:08:05 -0400

    Update games.js

--- games.js
+++ games.js
@@ -5,17 +5,23 @@ const productos = [{
         descripcion: ``,
         requisitos: ``
     },
-
     {
-        nombre: "grand thef auto",
-        link: `https://www.fghfg.com https://www.fghfg.com`,
-        ima: "img/Desert.jpg",
-        descripcion: "robo de autos",
-        requisitos: "una buena pc"
-    },
+        nombre: `Grand Theft Auto V`,
+        link: ``,
+        ima: `https://media.vandal.net/m/15192/grand-theft-auto-v-2015413122229_1.jpg`,
+        descripcion: `Cuando un joven estafador callejero, un ladrón de bancos retirado y un psicópata aterrador se ven envueltos en algunos de los elementos más aterradores y trastornados del inframundo criminal, el gobierno de los Estados Unidos y la industria del entretenimiento, deben llevar a cabo una serie de robos peligrosos para sobrevivir. en una ciudad despiadada en la que no pueden confiar en nadie, y menos en los demás.
+Grand Theft Auto V para PC ofrece a los jugadores la opción de explorar el galardonado mundo de Los Santos y el condado de Blaine en resoluciones de hasta 4k y más, así como la oportunidad de experimentar el juego corriendo a 60 cuadros por segundo.
 
-    { nombre: 'call of duty', link: "https://www.yjhtyhrhy.com", ima: 'img/animal2.png', descripcion: "dispros en primera persona", requisitos: "una buena pc" },
-    { nombre: 'need for speed', link: "https://www.ldsyrjyhjfdytfvfsd.com", ima: 'img/ciudad1.png', descripcion: "carrera callejeras", requisitos: "una buena pc" },
-    { nombre: 'metal geard solid', link: "https://www.yrejryej.com", ima: 'img/ciudad2.png', descripcion: "simulador de espionaje tactico", requisitos: "una buena pc" },
-    { nombre: 'assasins creed', link: "https://www.ryjejyreyjy.com", ima: 'img/naturaleza1.png', descripcion: "ecapuchos que matan en silencio", requisitos: "una buena pc" }
-]
+El juego ofrece a los jugadores una amplia gama de opciones de personalización específicas para PC, que incluyen más de 25 configuraciones configurables independientes para la calidad de la textura, sombreadores, teselación, suavizado y más, así como soporte y amplia personalización para controles de mouse y teclado. Las opciones adicionales incluyen un control deslizante de densidad de población para controlar el tráfico de automóviles y peatones, así como compatibilidad con monitores dobles y triples, compatibilidad con 3D y compatibilidad con el controlador plug-and-play.
+
+Grand Theft Auto V para PC también incluye Grand Theft Auto Online, con soporte para 30 jugadores y dos espectadores. Grand Theft Auto Online para PC incluirá todas las actualizaciones de juego existentes y el contenido creado por Rockstar lanzado desde el lanzamiento de Grand Theft Auto Online, incluidos los modos Heists y Adversary.`,
+        requisitos: `Minimum:
+• OS: Windows 8.1 64 Bit, Windows 8 64 Bit, Windows 7 64 Bit Service Pack 1, Windows Vista 64 Bit Service Pack 2* (*NVIDIA video card recommended if running Vista OS)
+• Processor: Intel Core 2 Quad CPU Q6600 @ 2.40GHz (4 CPUs) / AMD Phenom 9850 Quad-Core Processor (4 CPUs) @ 2.5GHz
+• Memory: 4 GB RAM
+• Graphics: NVIDIA 9800 GT 1GB / AMD HD 4870 1GB (DX 10, 10.1, 11)
+• Hard Drive: 65 GB available space
+• Sound Card: 100% DirectX 10 compatible`
+    },
+                  
+                  ]
