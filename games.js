11fe519b64e33a0d1786c1ec858d33b16fd7f4df
jmlc452 <71135625+jmlc452@users.noreply.github.com>
2020-10-14 11:39:50 -0400

    Update games.js

--- games.js
+++ games.js
@@ -9,19 +9,20 @@ const productos = [{
         nombre: `Grand Theft Auto V`,
         link: ``,
         ima: `https://media.vandal.net/m/15192/grand-theft-auto-v-2015413122229_1.jpg`,
-        descripcion: `Cuando un joven estafador callejero, un ladrón de bancos retirado y un psicópata aterrador se ven envueltos en algunos de los elementos más aterradores y trastornados del inframundo criminal, el gobierno de los Estados Unidos y la industria del entretenimiento, deben llevar a cabo una serie de robos peligrosos para sobrevivir. en una ciudad despiadada en la que no pueden confiar en nadie, y menos en los demás.
-Grand Theft Auto V para PC ofrece a los jugadores la opción de explorar el galardonado mundo de Los Santos y el condado de Blaine en resoluciones de hasta 4k y más, así como la oportunidad de experimentar el juego corriendo a 60 cuadros por segundo.
-
-El juego ofrece a los jugadores una amplia gama de opciones de personalización específicas para PC, que incluyen más de 25 configuraciones configurables independientes para la calidad de la textura, sombreadores, teselación, suavizado y más, así como soporte y amplia personalización para controles de mouse y teclado. Las opciones adicionales incluyen un control deslizante de densidad de población para controlar el tráfico de automóviles y peatones, así como compatibilidad con monitores dobles y triples, compatibilidad con 3D y compatibilidad con el controlador plug-and-play.
-
-Grand Theft Auto V para PC también incluye Grand Theft Auto Online, con soporte para 30 jugadores y dos espectadores. Grand Theft Auto Online para PC incluirá todas las actualizaciones de juego existentes y el contenido creado por Rockstar lanzado desde el lanzamiento de Grand Theft Auto Online, incluidos los modos Heists y Adversary.`,
-        requisitos: "Minimum:
-• OS: Windows 8.1 64 Bit, Windows 8 64 Bit, Windows 7 64 Bit Service Pack 1, Windows Vista 64 Bit Service Pack 2* (*NVIDIA video card recommended if running Vista OS)/n
-• Processor: Intel Core 2 Quad CPU Q6600 @ 2.40GHz (4 CPUs) / AMD Phenom 9850 Quad-Core Processor (4 CPUs) @ 2.5GHz
-• Memory: 4 GB RAM
-• Graphics: NVIDIA 9800 GT 1GB / AMD HD 4870 1GB (DX 10, 10.1, 11)
-• Hard Drive: 65 GB available space
-• Sound Card: 100% DirectX 10 compatible"
+        descripcion: ``,
+        requisitos: ``
     },
-                  
+      {
+        nombre: `Grand Theft Auto IV`,
+        link: ``,
+        ima: `https://vignette.wikia.nocookie.net/esgta/images/d/de/Grand_Theft_Auto_IV.JPG/revision/latest?cb=20080911024224`,
+        descripcion: ``,
+        requisitos: ``
+    },{
+        nombre: `Grand Theft Auto III`,
+        link: ``,
+        ima: `https://vignette.wikia.nocookie.net/esgta/images/a/a2/GTAIII_car%C3%A1tula.png/revision/latest?cb=20160125000236`,
+        descripcion: ``,
+        requisitos: ``
+    },            
                   ]
