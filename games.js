7c35f907f61141e1e56a3c44e095deb2fbcc5f20
jmlc452 <71135625+jmlc452@users.noreply.github.com>
2020-10-14 11:13:26 -0400

    Update games.js

--- games.js
+++ games.js
@@ -15,13 +15,13 @@ Grand Theft Auto V para PC ofrece a los jugadores la opción de explorar el gala
 El juego ofrece a los jugadores una amplia gama de opciones de personalización específicas para PC, que incluyen más de 25 configuraciones configurables independientes para la calidad de la textura, sombreadores, teselación, suavizado y más, así como soporte y amplia personalización para controles de mouse y teclado. Las opciones adicionales incluyen un control deslizante de densidad de población para controlar el tráfico de automóviles y peatones, así como compatibilidad con monitores dobles y triples, compatibilidad con 3D y compatibilidad con el controlador plug-and-play.
 
 Grand Theft Auto V para PC también incluye Grand Theft Auto Online, con soporte para 30 jugadores y dos espectadores. Grand Theft Auto Online para PC incluirá todas las actualizaciones de juego existentes y el contenido creado por Rockstar lanzado desde el lanzamiento de Grand Theft Auto Online, incluidos los modos Heists y Adversary.`,
-        requisitos: `Minimum:
+        requisitos: "Minimum:
 • OS: Windows 8.1 64 Bit, Windows 8 64 Bit, Windows 7 64 Bit Service Pack 1, Windows Vista 64 Bit Service Pack 2* (*NVIDIA video card recommended if running Vista OS)/n
 • Processor: Intel Core 2 Quad CPU Q6600 @ 2.40GHz (4 CPUs) / AMD Phenom 9850 Quad-Core Processor (4 CPUs) @ 2.5GHz
 • Memory: 4 GB RAM
 • Graphics: NVIDIA 9800 GT 1GB / AMD HD 4870 1GB (DX 10, 10.1, 11)
 • Hard Drive: 65 GB available space
-• Sound Card: 100% DirectX 10 compatible`
+• Sound Card: 100% DirectX 10 compatible"
     },
                   
                   ]
