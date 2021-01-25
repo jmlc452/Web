83a96b05be8b48144d3cfe7560ecaa3ab8eaebf5
jmlc452 <71135625+jmlc452@users.noreply.github.com>
2021-01-25 10:23:37 -0400

    Update main.js

--- main.js
+++ main.js
@@ -78,7 +78,7 @@ fetch('res/texto.txt')
     .then(res => res.text())
     .then(content => {
         let lines = content.split(/\n/);
-        lines.forEach(line => document.getElementById('textmega').innerHTML="line");
+        lines.forEach(line => document.getElementById('textmega').innerHTML=line);
     })
 
 function copiarAlPortapapeles(id_elemento) {
